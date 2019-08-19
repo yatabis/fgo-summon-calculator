e604be080ab3444776f23f9849c0d72759c26c1a
yatabis <39yatabis.rim8820xxx@gmail.com>
2019-08-19 11:08:10 +0900

    update: 更新順の記憶

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const vm = new Vue({
   data: {
     saint_quartz: 0,
     summon_ticket: 0,
-    summon: 0
+    summon: 0,
+    rarity: 5,
+    latest: {
+      saint_quartz: 0,
+      summon_ticket: 1,
+      summon: 2
+    }
   },
   watch: {
     saint_quartz: function() {
@@ -14,6 +20,13 @@ const vm = new Vue({
     }
   },
   methods: {
+    updateLatest: function(key) {
+      for (let k of Object.keys(this.latest)) {
+        this.latest[k]++
+      }
+      this.latest[key] = 0;
+      for (let k of Object.keys(this.latest)) console.log(k, this.latest[k]);
+    },
     updateSummon: function() {
       this.summon = Math.floor((Math.floor(this.saint_quartz / 3) + Number(this.summon_ticket)) * 1.1)
     }
