2fd892c08a33dfff02abd7516b03c6b8d161612e
yatabis <39yatabis.rim8820xxx@gmail.com>
2019-08-19 12:00:48 +0900

    add: 確率を計算

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
+const combination = (a, b) => {
+  let c = 1
+  for (let i = 1; i <= b; i++) {
+    c = c * (a - i + 1) / i
+  }
+  return c
+}
+
 const vm = new Vue({
   el: "#main",
   data: {
-    saint_quartz: 0,
+    saint_quartz: 30,
     summon_ticket: 0,
     rarity: 5,
     np_level: 1,
@@ -20,10 +28,18 @@ const vm = new Vue({
         this.saint_quartz = (Math.floor(summon / 11) * 10 + summon % 11) * 3
         this.summon_ticket = 0
       }
+    },
+    hit: function () {
+      return 0.008
+    },
+    probability: {
+      get: function () {
+        return combination(this.summon, this.np_level) * this.hit**this.np_level * (1 - this.hit) ** (this.summon - this.np_level)
+      }
     }
   },
   methods: {
-    updateLatest: function(key) {
+    updateLatest: function (key) {
       for (let k of Object.keys(this.latest)) {
         this.latest[k]++
       }
